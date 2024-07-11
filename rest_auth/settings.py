981e3ac45667d94a9b3fa8f94db33377f22ffd00
soaqa <soaqa57@gmail.com>
2024-07-11 20:46:38 +0300

    djoser

--- rest_auth/settings.py
+++ rest_auth/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'api',
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -126,6 +128,13 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': ["rest_framework.authentication.SessionAuthentication"],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        "rest_framework.authentication.SessionAuthentication",
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
     'DEFAULT_PERMISSION_CLASSES': ["rest_framework.permissions.IsAuthenticated"],
+}
+
+SIMPLE_JWT = {
+   'AUTH_HEADER_TYPES': ('SAS',),
 }
